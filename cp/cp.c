6f52348948966d51103b0217116d896f77ef606f
Ali Ghaffarian <alighaffarian9@gmail.com>
2024-06-20 18:49:23 +0330

    arg handling and added more options

--- cp/cp.c
+++ cp/cp.c
@@ -2,50 +2,117 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #define INVALID_ARGS_MSG "invalid args"
-#define BUFFERSIZE 128
 #define FD_ARR_SIZE 2
-void finish_and_exit(int* fd_to_close, int err_code){
+#define OPEN_FILE_ERR_MSG "error during openning file"
+
+#define DEBUG_PREFIX "DEBUG : "
+#define VERBOSE_PREFIX "VERBOSE : "
+
+int block_size = 128;
+int do_debug = 0;
+int do_verbose = 0;
+int overwrite = 0;
+char* src_path = NULL;
+char* dst_path = NULL;
+
+/*
+	options to do:
+	verbose and do_debug
+	force ( overwrite )
+	check for overwrite and prompt
+	need to have the paths after all the options
+*/
+
+void finish_and_exit(int* fd_to_close){
 	int i = 0;
 	while(i < FD_ARR_SIZE){
 			close(fd_to_close[i++]);
 	}
-	if (err_code){
-			printf("err occured during something, err code : %d\n", err_code);
-	}
-	exit(err_code);
+	exit(errno);
 }
-int main(int argc, char** argv){
 
+
+void init_args(int argc, char** argv){
+		int opt;
+		while((opt = getopt(argc, argv, "dvfb:")) != -1){
+				switch(opt){
+						case 'd':{
+										 do_debug = 1;
+										 break;
+								 }
+						case 'v':{
+										 do_debug = 1;
+										 break;
+								 }
+						case 'b':{
+										 block_size = atoi(optarg);
+										 break;
+								 }
+						case 'f':{
+										 overwrite = 1;
+										 break;
+								 }
+						default:break;
+				}
+		}
+
+		src_path = argv[argc - 2];
+		dst_path = argv[argc - 1];
+
+		if (do_debug){
+				printf("%sdebug flag : %d\n", DEBUG_PREFIX ,do_debug);
+				printf("%sverbose flag : %d\n", DEBUG_PREFIX, do_verbose);
+				printf("%soverwrite flag : %d\n", DEBUG_PREFIX, overwrite);
+				printf("%sblock size : %d\n", DEBUG_PREFIX, block_size);
+				printf("%ssource path : %s\n", DEBUG_PREFIX, src_path);
+				printf("%sdestination path : %s\n", DEBUG_PREFIX, dst_path);
+		}
+} 
+
+int main(int argc, char** argv){
+		init_args(argc, argv);
 		char* paths[FD_ARR_SIZE];
 		int fd_list[FD_ARR_SIZE];
 
-		int src = open(argv[1], O_RDONLY);
-		paths[0] = argv[1];
+		int src = open(src_path, O_RDONLY);
+		if(src == -1){
+				printf("%s", OPEN_FILE_ERR_MSG);
+				printf(": %s\n", src_path);
+				finish_and_exit(fd_list);
+		}
+
 		fd_list[0] = src;
 
-		int dst = open(argv[2], O_WRONLY | O_CREAT);
-		paths[1] = argv[2];
+		int dst = open(dst_path, O_WRONLY | O_CREAT);
+		if(src == -1){
+				printf("%s", OPEN_FILE_ERR_MSG);
+				printf(": %s\n", dst_path);
+				finish_and_exit(fd_list);
+		}
+
+
 		fd_list[1] = dst;
 
-		char buff[BUFFERSIZE];
+		char buff[block_size];
 		int read_bytes = 0;
 		int write_bytes = 0;
 
 		while (1){
-				read_bytes = read( src, buff, BUFFERSIZE);
+				read_bytes = read( src, buff, block_size );
 				if(read_bytes == -1){
-						printf("err during reading from %s\n", argv[1]);
-						finish_and_exit(fd_list, 1);
+						printf("err during reading from %s\n", src_path);
+						finish_and_exit(fd_list);
 				}
 
 				write_bytes = write( dst, buff, read_bytes);
 				if(write_bytes == -1){
-						printf("err during writing to %s\n", argv[2]);
-						finish_and_exit(fd_list, 1);
+						printf("err during writing to %s\n", dst_path);
+						finish_and_exit(fd_list);
 				}
-				if (read_bytes != BUFFERSIZE){
-						finish_and_exit(fd_list, 0);
+				if (read_bytes != block_size || read_bytes == 0){
+						finish_and_exit(fd_list);
 				}
 				printf("transfered %d bytes\n", read_bytes);
 		}
